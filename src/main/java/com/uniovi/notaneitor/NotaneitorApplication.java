e06dd8cd34f3966314dce54215ce4e5261d87d95
Celia <uo276077@uniovi.es>
2022-02-17 20:48:23 +0100

    SDI-2122-315-2.2-Trabajando con Controladores.

--- src/main/java/com/uniovi/notaneitor/NotaneitorApplication.java
+++ src/main/java/com/uniovi/notaneitor/NotaneitorApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Sdi2122101LabSpringApplication {
+public class NotaneitorApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Sdi2122101LabSpringApplication.class, args);
+        SpringApplication.run(NotaneitorApplication.class, args);
     }
 
 }
