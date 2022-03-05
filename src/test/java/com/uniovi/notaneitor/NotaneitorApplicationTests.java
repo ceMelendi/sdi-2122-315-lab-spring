3ffe5819767bc708691e35842c19398340c33f86
Celia <uo276077@uniovi.es>
2022-03-05 19:19:55 +0100

    SDI-2122-315-5.1-Clase principal.

--- src/test/java/com/uniovi/notaneitor/NotaneitorApplicationTests.java
+++ src/test/java/com/uniovi/notaneitor/NotaneitorApplicationTests.java
@@ -6,12 +6,13 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class NotaneitorApplicationTests {
 
-    static String PathFirefox = "C:\\Program Files\\Mozilla Firefox\\firefox.exe"; //CLASE
-    static String Geckodriver = "C:\\Users\\uo276077\\Desktop\\SDI\\PL-SDI-Sesión5-material\\geckodriver-v0.30.0-win64.exe"; //CLASE
+    static String PathFirefox = "C:\\Program Files\\Mozilla Firefox\\firefox.exe"; //CLASE y CASA
+    //static String Geckodriver = "C:\\Users\\uo276077\\Desktop\\SDI\\PL-SDI-Sesión5-material\\geckodriver-v0.30.0-win64.exe"; //CLASE
+    static String Geckodriver = "E:\\UNIOVI\\TERCERO\\Segundo cuatri\\SDI\\PL-SDI-Sesión5-material\\geckodriver-v0.30.0-win64.exe"; //CASA
     //static String Geckodriver = "C:\\Dev\\tools\\selenium\\geckodriver-v0.30.0-win64.exe";
-    //static String PathFirefox = "/Applications/Firefox.app/Contents/MacOS/firefox-bin";
     //static String Geckodriver = "/Users/USUARIO/selenium/geckodriver-v0.30.0-macos";
 
     //Común a Windows y a MACOSX
@@ -43,4 +44,20 @@ class NotaneitorApplicationTests {
         //Cerramos el navegador al finalizar las pruebas
         driver.quit();
     }
+
+    @Test
+    @Order(1)
+    void PR01(){}
+
+    @Test
+    @Order(2)
+    void PR02(){}
+
+    @Test
+    @Order(3)
+    void PR03(){}
+
+    @Test
+    @Order(4)
+    void PR04(){}
 }
